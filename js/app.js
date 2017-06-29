a80e828fb5ed2eed73038b0453705fd90d8c1673
Raunak Sett <sett.raunak@gmail.com>
2017-06-29 22:00:14 +0530

    Update 2017-06-29T16:30:09.548Z

--- js/app.js
+++ js/app.js
@@ -105,6 +105,49 @@
 
 (function () {
     'use strict';
+    //@todo Move this service to the common folder
+    angular.module('selfService')
+        .service('BeneficiariesService', ['$q', '$http', '$rootScope', '$resource', 'BASE_URL', BeneficiariesService]);
+
+    function BeneficiariesService($q, $http, $rootScope, $resource, BASE_URL) {
+
+        this.getBeneficiaries = function () {
+            return $resource(BASE_URL + '/self/beneficiaries/tpt');
+        };
+    }
+
+})();
+
+(function () {
+    'use strict';
+
+    angular.module('selfService')
+        .controller('BeneficiariesListCtrl', ['$scope', '$rootScope', '$stateParams', 'BeneficiariesService', BeneficiariesListCtrl]);
+
+    function BeneficiariesListCtrl($scope, $rootScope, $stateParams, BeneficiariesService) {
+
+        var vm = this;
+        vm.loadingBeneficiaries = true;
+        vm.beneficiaries = [];
+        vm.beneficiariesFilter = "";
+        vm.page = 1;
+        vm.query = {
+            limit: 5,
+            offset: 0
+        }
+
+        vm.getBeneficiaries = getBeneficiaries();
+
+        function getBeneficiaries() {
+            BeneficiariesService.getBeneficiaries().query().$promise.then(function(data) {
+                vm.beneficiaries = data;
+                vm.loadingBeneficiaries = false;
+            });
+        }
+    }
+})();
+(function () {
+    'use strict';
     angular.module('selfService')
         .service('AccountTransferService', ['$q', '$http', '$rootScope', '$resource', 'BASE_URL', AccountTransferService]);
 
