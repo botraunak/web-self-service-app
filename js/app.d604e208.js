1b6232b36aa33e153cbf3176068a6791ebd82692
Raunak Sett <sett.raunak@gmail.com>
2017-08-16 18:26:49 +0530

    Update 2017-08-16T12:56:37.438Z

--- js/app.d604e208.js
+++ js/app.d604e208.js
@@ -874,6 +874,98 @@
     'use strict';
     //@todo Move this service to the common folder
     angular.module('selfService')
+        .service('LoanAccountService', ['$resource', 'BASE_URL', LoanAccountService]);
+
+    function LoanAccountService($resource, BASE_URL) {
+
+        this.loanAccount = function () {
+            return $resource(BASE_URL + '/self/loans/:id',{id: '@id'});
+        }
+    }
+
+})();
+
+
+(function () {
+    'use strict';
+
+    angular.module('selfService')
+        .controller('LoanAccountViewCtrl', ['$state', '$stateParams', '$filter', 'LoanAccountService', LoanAccountViewCtrl]);
+
+    /**
+     * @module LoanAccountViewCtrl
+     * @description
+     * Handles the Loan Account Details Page.
+     */
+    function LoanAccountViewCtrl($state, $stateParams, $filter, LoanAccountService) {
+
+        var vm = this;
+
+        /**
+         * @name loadingLoanAccountInfo
+         * @description flag to check whether account info is loaded or not
+         * @type {boolean}
+         */
+        vm.loadingLoanAccountInfo = true;
+
+        /**
+         * @name loanAccountDetails
+         * @type {object}
+         * @description To store the loan Account details returned by server
+         */
+        vm.loanAccountDetails = getLoanDetails($stateParams.id);
+
+        /**
+         * @name statusClass
+         * @type {string}
+         * @description To store the css class for loan status [active, pending, ...]
+         */
+        vm.statusClass = '';
+
+        vm.repaymentSchedule = {};
+
+        vm.makePayment = makePayment;
+
+        /**
+         * @method getLoanDetails
+         * @description To get the loan details from the server
+         * @param id {number} Loan Account id
+         */
+        function getLoanDetails(id) {
+            LoanAccountService.loanAccount().get({
+                id: id,
+                associations:'repaymentSchedule,transactions'
+            }).$promise.then(function (res) {
+                vm.loadingLoanAccountInfo = false;
+                vm.loanAccountDetails = res;
+                getStatusClass();
+            });
+        }
+
+        /**
+         * @method getStatusClass
+         * @description To get the loan account status through the status lookup filter
+         */
+        function getStatusClass() {
+            var statusClass = $filter('StatusLookup')(vm.loanAccountDetails.status.code);
+            statusClass = 'bg_' + statusClass;
+            if (vm.loanAccountDetails.inArrears) {
+                statusClass += 'overdue';
+            }
+            vm.statusClass = statusClass;
+        }
+
+        function makePayment() {
+            $state.go('app.transfers', {
+                toAccount: vm.loanAccountDetails
+            });
+        }
+    }
+})();
+(function () {
+    'use strict';
+    //@todo Move this service to the common folder
+    angular.module('selfService')
         .service('SavingsAccountService', ['$resource', 'BASE_URL', SavingsAccountService]);
 
     function SavingsAccountService($resource, BASE_URL) {
@@ -971,98 +1063,6 @@
 				});
 			}
 		}
-})();
-(function () {
-    'use strict';
-    //@todo Move this service to the common folder
-    angular.module('selfService')
-        .service('LoanAccountService', ['$resource', 'BASE_URL', LoanAccountService]);
-
-    function LoanAccountService($resource, BASE_URL) {
-
-        this.loanAccount = function () {
-            return $resource(BASE_URL + '/self/loans/:id',{id: '@id'});
-        }
-    }
-
-})();
-
-
-(function () {
-    'use strict';
-
-    angular.module('selfService')
-        .controller('LoanAccountViewCtrl', ['$state', '$stateParams', '$filter', 'LoanAccountService', LoanAccountViewCtrl]);
-
-    /**
-     * @module LoanAccountViewCtrl
-     * @description
-     * Handles the Loan Account Details Page.
-     */
-    function LoanAccountViewCtrl($state, $stateParams, $filter, LoanAccountService) {
-
-        var vm = this;
-
-        /**
-         * @name loadingLoanAccountInfo
-         * @description flag to check whether account info is loaded or not
-         * @type {boolean}
-         */
-        vm.loadingLoanAccountInfo = true;
-
-        /**
-         * @name loanAccountDetails
-         * @type {object}
-         * @description To store the loan Account details returned by server
-         */
-        vm.loanAccountDetails = getLoanDetails($stateParams.id);
-
-        /**
-         * @name statusClass
-         * @type {string}
-         * @description To store the css class for loan status [active, pending, ...]
-         */
-        vm.statusClass = '';
-
-        vm.repaymentSchedule = {};
-
-        vm.makePayment = makePayment;
-
-        /**
-         * @method getLoanDetails
-         * @description To get the loan details from the server
-         * @param id {number} Loan Account id
-         */
-        function getLoanDetails(id) {
-            LoanAccountService.loanAccount().get({
-                id: id,
-                associations:'repaymentSchedule,transactions'
-            }).$promise.then(function (res) {
-                vm.loadingLoanAccountInfo = false;
-                vm.loanAccountDetails = res;
-                getStatusClass();
-            });
-        }
-
-        /**
-         * @method getStatusClass
-         * @description To get the loan account status through the status lookup filter
-         */
-        function getStatusClass() {
-            var statusClass = $filter('StatusLookup')(vm.loanAccountDetails.status.code);
-            statusClass = 'bg_' + statusClass;
-            if (vm.loanAccountDetails.inArrears) {
-                statusClass += 'overdue';
-            }
-            vm.statusClass = statusClass;
-        }
-
-        function makePayment() {
-            $state.go('app.transfers', {
-                toAccount: vm.loanAccountDetails
-            });
-        }
-    }
 })();
 
 (function () {
